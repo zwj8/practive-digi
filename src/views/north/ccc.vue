d27a0e22624e40d4cd99f8ef3b675cbe5639c6c1
zwj <1448219795@qq.com>
2022-12-17 08:01:07 +0800

    ccc小伙伴加入进来了

---
+++ src/views/north/ccc.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        ccc小伙伴加入进来了
+    </div>
+</template>
+<script>
+export default {
+    name: '',
+    data() {
+        return {
+
+        }
+    },
+    created() {
+    },
+    computed: {
+    },
+    methods: {
+    }
+}
+</script>
+<style lang='less'  scoped>
+
+</style>
